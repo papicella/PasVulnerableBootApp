fc6d124b81205a499c423e385d45b068a75e431b
papicella <pas.apicella@gmail.com>
2025-10-06 14:39:58 +1100

    Initial commit

---
+++ src/main/java/com/example/pasvulnerablebootapp/PasVulnerableBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.pasvulnerablebootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasVulnerableBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasVulnerableBootAppApplication.class, args);
+    }
+
+}
